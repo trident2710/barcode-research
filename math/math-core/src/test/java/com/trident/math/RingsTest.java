e87e7795658051749e0d5d227bd0d9f1b7bf6832
Andrii Dychka <andrii.dychka@playtech.com>
2020-07-21 18:04:37 +0300

    Static import

--- math/math-core/src/test/java/com/trident/math/RingsTest.java
+++ math/math-core/src/test/java/com/trident/math/RingsTest.java
@@ -1,8 +1,9 @@
 package com.trident.math;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import cc.redberry.rings.IntegersZp64;
 import cc.redberry.rings.Rings;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class RingsTest {
@@ -11,28 +12,28 @@ class RingsTest {
 
     @Test
     void testAdd() {
-        Assertions.assertEquals(2, GF5.add(3, 4));
+        assertEquals(2, GF5.add(3, 4));
     }
 
     @Test
     void testDiv() {
-        Assertions.assertEquals(3, GF5.divide(4, 3));
+        assertEquals(3, GF5.divide(4, 3));
     }
 
     @Test
     void testMul() {
-        Assertions.assertEquals(1, GF5.multiply(2, 3));
+        assertEquals(1, GF5.multiply(2, 3));
     }
 
     @Test
     void testSub() {
-        Assertions.assertEquals(3, GF5.subtract(2, 4));
+        assertEquals(3, GF5.subtract(2, 4));
     }
 
     @Test
     void testModulus() {
-        Assertions.assertEquals(1, GF5.modulus(11));
-        Assertions.assertEquals(3, GF5.modulus(28));
-        Assertions.assertEquals(4, GF5.modulus(9));
+        assertEquals(1, GF5.modulus(11));
+        assertEquals(3, GF5.modulus(28));
+        assertEquals(4, GF5.modulus(9));
     }
 }
