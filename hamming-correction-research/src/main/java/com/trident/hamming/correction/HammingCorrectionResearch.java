0e19926de097701e5cce49c2eb4ed4c87b1a64b5
Andrii Dychka <andrii.dychka@playtech.com>
2020-07-22 14:42:23 +0300

    Add CLI to HammingCorrectionResearch app

--- hamming-correction-research/src/main/java/com/trident/hamming/correction/HammingCorrectionResearch.java
+++ hamming-correction-research/src/main/java/com/trident/hamming/correction/HammingCorrectionResearch.java
@@ -1,31 +1,37 @@
 package com.trident.hamming.correction;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.trident.hamming.correction.service.HammingCodeReader;
 import com.trident.hamming.correction.service.HammingCorrectionAnalyzer;
-import com.trident.math.hamming.HammingCode;
-import com.trident.math.io.converter.HammingCodeConverter;
-import com.trident.math.io.dto.HammingCodeDto;
-
-import java.net.URL;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class HammingCorrectionResearch {
 
+    private static final Options OPTIONS = new Options()
+            .addOption("l", "errorLevel", true, "Error level")
+            .addOption("c", "hammingCode", true, "Hamming code description file path")
+            .addOption("i", "iterations", true, "Number of iterations to perform");
+
     public static void main(String[] args) throws Exception {
-        var hammingCode = hammingCode();
-        var result = HammingCorrectionAnalyzer.analyzeHammingCodeCorrection(hammingCode, 2, 100000);
+        try {
+            var cmd = new DefaultParser().parse(OPTIONS, args);
+            run(cmd);
+        } catch (ParseException ex) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("HammingCorrectionResearch", OPTIONS);
+        }
+    }
+
+    private static void run(CommandLine commandLine) throws Exception {
+        int errorLevel = Integer.parseInt(commandLine.getOptionValue("l"));
+        int iterations = Integer.parseInt(commandLine.getOptionValue("i"));
+        String hammingCodePath = commandLine.getOptionValue("c");
+
+        var hammingCode = HammingCodeReader.read(hammingCodePath);
+        var result = HammingCorrectionAnalyzer.analyzeHammingCodeCorrection(hammingCode, errorLevel, iterations);
         System.out.println(result);
-    }
-
-    private static HammingCode hammingCode() throws Exception {
-        URL file = HammingCorrectionResearch.class.getClassLoader().getResource("hamming-sample.json");
-        var dto = objectMapper().readValue(file, HammingCodeDto.class);
-        return HammingCodeConverter.fromDto(dto);
-    }
-
-    private static ObjectMapper objectMapper() {
-        var objectMapper = new ObjectMapper();
-        objectMapper.registerModules(new GuavaModule());
-        return objectMapper;
     }
 }
---
+++ hamming-correction-research/src/main/java/com/trident/hamming/correction/service/HammingCodeReader.java
@@ -0,0 +1,24 @@
+package com.trident.hamming.correction.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.trident.math.hamming.HammingCode;
+import com.trident.math.io.converter.HammingCodeConverter;
+import com.trident.math.io.dto.HammingCodeDto;
+
+import java.io.File;
+
+public class HammingCodeReader {
+
+    public static HammingCode read(String path) throws Exception {
+        File file = new File(path);
+        var dto = objectMapper().readValue(file, HammingCodeDto.class);
+        return HammingCodeConverter.fromDto(dto);
+    }
+
+    private static ObjectMapper objectMapper() {
+        var objectMapper = new ObjectMapper();
+        objectMapper.registerModules(new GuavaModule());
+        return objectMapper;
+    }
+}
