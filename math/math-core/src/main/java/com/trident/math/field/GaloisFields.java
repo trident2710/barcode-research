8e537f207f0d975d817dd071ea95e751e7e3289e
Andrii Dychka <andrii.dychka@playtech.com>
2021-02-13 15:05:12 +0200

    Add HammingCodes

--- math/math-core/src/main/java/com/trident/math/field/GaloisFields.java
+++ math/math-core/src/main/java/com/trident/math/field/GaloisFields.java
@@ -6,7 +6,11 @@ public final class GaloisFields {
 
     public static final GaloisFieldOverPrime GF5 = GaloisFieldOverPrime.of(5);
 
+    public static final GaloisFieldOverPrime GF7 = GaloisFieldOverPrime.of(7);
+
     public static GaloisFieldOverPoly GF_2_2 = GaloisFieldOverPoly.of(2, 2, new long[]{1, 1, 1});
+
+    public static GaloisFieldOverPoly GF_2_3 = GaloisFieldOverPoly.of(2, 3, new long[]{1, 1, 0, 1});
 
     public static GaloisFieldOverPoly GF_3_2 = GaloisFieldOverPoly.of(3, 2, new long[]{2, 1, 1});
 
