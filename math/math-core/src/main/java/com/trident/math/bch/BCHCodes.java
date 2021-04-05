ded0d2bb412418b41d5708b9486927681213d94c
Andrii Dychka <andrii.dychka@playtech.com>
2021-04-05 22:03:39 +0300

    Add BCH codes

--- math/math-core/src/main/java/com/trident/math/bch/BCHCodes.java
+++ math/math-core/src/main/java/com/trident/math/bch/BCHCodes.java
@@ -7,6 +7,7 @@ import com.trident.math.field.GFPMSimpleElement;
 import static com.trident.math.field.GaloisFields.GF3;
 import static com.trident.math.field.GaloisFields.GF_2_2;
 import static com.trident.math.field.GaloisFields.GF_3_2;
+import static com.trident.math.field.GaloisFields.GF_3_3;
 import static com.trident.math.field.GaloisFields.GF_4_2;
 import static com.trident.math.matrix.GaloisFieldMatrixUtil.toFieldMatrix;
 
@@ -60,4 +61,116 @@ public final class BCHCodes {
                     new long[]{1, 14, 8, 10, 13, 1, 14, 8, 10, 13, 1},
                     new long[]{1, 5, 7, 13, 4, 2, 10, 9, 6, 8, 3},
             }, GF_4_2), GF_4_2.getClass());
+
+    public static BCHCode<GFPElement, GFPMSimpleElement> BCH_18_9 = new BCHCode<>(
+            toFieldMatrix(new long[][]{
+                    new long[]{1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                    new long[]{0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0},
+                    new long[]{0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0},
+                    new long[]{0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0},
+                    new long[]{0, 0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0, 0},
+                    new long[]{0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0},
+                    new long[]{0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0},
+                    new long[]{0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0},
+                    new long[]{0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1},
+            }, GF3), GF3.getClass(),
+            toFieldMatrix(new long[][]{
+                    new long[]{1, 3, 9, 5, 15, 23, 13, 17, 20, 4, 12, 14, 11, 2, 6, 18, 7, 21},
+                    new long[]{1, 9, 15, 13, 20, 12, 11, 6, 7, 16, 22, 8, 25, 1, 9, 15, 13, 20},
+                    new long[]{1, 5, 13, 4, 11, 18, 16, 10, 25, 3, 15, 17, 12, 2, 7, 26, 8, 19},
+                    new long[]{1, 15, 20, 11, 7, 22, 25, 9, 13, 12, 6, 16, 8, 1, 15, 20, 11, 7},
+            }, GF_3_3), GF_3_3.getClass());
+
+    public static BCHCode<GFPElement, GFPMSimpleElement> BCH_17_8 = new BCHCode<>(
+            toFieldMatrix(new long[][]{
+                    new long[]{1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0},
+                    new long[]{0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0},
+                    new long[]{0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0},
+                    new long[]{0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0, 0},
+                    new long[]{0, 0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0},
+                    new long[]{0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0},
+                    new long[]{0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0},
+                    new long[]{0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1},
+            }, GF3), GF3.getClass(),
+            toFieldMatrix(new long[][]{
+                    new long[]{1, 3, 9, 5, 15, 23, 13, 17, 20, 4, 12, 14, 11, 2, 6, 18, 7},
+                    new long[]{1, 9, 15, 13, 20, 12, 11, 6, 7, 16, 22, 8, 25, 1, 9, 15, 13},
+                    new long[]{1, 5, 13, 4, 11, 18, 16, 10, 25, 3, 15, 17, 12, 2, 7, 26, 8},
+                    new long[]{1, 15, 20, 11, 7, 22, 25, 9, 13, 12, 6, 16, 8, 1, 15, 20, 11},
+            }, GF_3_3), GF_3_3.getClass());
+
+    public static BCHCode<GFPElement, GFPMSimpleElement> BCH_16_7 = new BCHCode<>(
+            toFieldMatrix(new long[][]{
+                    new long[]{1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0},
+                    new long[]{0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0},
+                    new long[]{0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0, 0},
+                    new long[]{0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0},
+                    new long[]{0, 0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0},
+                    new long[]{0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0},
+                    new long[]{0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1},
+            }, GF3), GF3.getClass(),
+            toFieldMatrix(new long[][]{
+                    new long[]{1, 3, 9, 5, 15, 23, 13, 17, 20, 4, 12, 14, 11, 2, 6, 18},
+                    new long[]{1, 9, 15, 13, 20, 12, 11, 6, 7, 16, 22, 8, 25, 1, 9, 15},
+                    new long[]{1, 5, 13, 4, 11, 18, 16, 10, 25, 3, 15, 17, 12, 2, 7, 26},
+                    new long[]{1, 15, 20, 11, 7, 22, 25, 9, 13, 12, 6, 16, 8, 1, 15, 20},
+            }, GF_3_3), GF_3_3.getClass());
+
+    public static BCHCode<GFPElement, GFPMSimpleElement> BCH_15_6 = new BCHCode<>(
+            toFieldMatrix(new long[][]{
+                    new long[]{1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0},
+                    new long[]{0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0, 0},
+                    new long[]{0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0},
+                    new long[]{0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0},
+                    new long[]{0, 0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0},
+                    new long[]{0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1},
+            }, GF3), GF3.getClass(),
+            toFieldMatrix(new long[][]{
+                    new long[]{1, 3, 9, 5, 15, 23, 13, 17, 20, 4, 12, 14, 11, 2, 6},
+                    new long[]{1, 9, 15, 13, 20, 12, 11, 6, 7, 16, 22, 8, 25, 1, 9},
+                    new long[]{1, 5, 13, 4, 11, 18, 16, 10, 25, 3, 15, 17, 12, 2, 7},
+                    new long[]{1, 15, 20, 11, 7, 22, 25, 9, 13, 12, 6, 16, 8, 1, 15},
+            }, GF_3_3), GF_3_3.getClass());
+
+    public static BCHCode<GFPElement, GFPMSimpleElement> BCH_14_5 = new BCHCode<>(
+            toFieldMatrix(new long[][]{
+                    new long[]{1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0, 0},
+                    new long[]{0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0},
+                    new long[]{0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0},
+                    new long[]{0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0},
+                    new long[]{0, 0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1},
+            }, GF3), GF3.getClass(),
+            toFieldMatrix(new long[][]{
+                    new long[]{1, 3, 9, 5, 15, 23, 13, 17, 20, 4, 12, 14, 11, 2},
+                    new long[]{1, 9, 15, 13, 20, 12, 11, 6, 7, 16, 22, 8, 25, 1},
+                    new long[]{1, 5, 13, 4, 11, 18, 16, 10, 25, 3, 15, 17, 12, 2},
+                    new long[]{1, 15, 20, 11, 7, 22, 25, 9, 13, 12, 6, 16, 8, 1},
+            }, GF_3_3), GF_3_3.getClass());
+
+    public static BCHCode<GFPElement, GFPMSimpleElement> BCH_13_4 = new BCHCode<>(
+            toFieldMatrix(new long[][]{
+                    new long[]{1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0, 0},
+                    new long[]{0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0},
+                    new long[]{0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0},
+                    new long[]{0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1},
+            }, GF3), GF3.getClass(),
+            toFieldMatrix(new long[][]{
+                    new long[]{1, 3, 9, 5, 15, 23, 13, 17, 20, 4, 12, 14, 11},
+                    new long[]{1, 9, 15, 13, 20, 12, 11, 6, 7, 16, 22, 8, 25},
+                    new long[]{1, 5, 13, 4, 11, 18, 16, 10, 25, 3, 15, 17, 12},
+                    new long[]{1, 15, 20, 11, 7, 22, 25, 9, 13, 12, 6, 16, 8},
+            }, GF_3_3), GF_3_3.getClass());
+
+    public static BCHCode<GFPElement, GFPMSimpleElement> BCH_12_3 = new BCHCode<>(
+            toFieldMatrix(new long[][]{
+                    new long[]{1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0},
+                    new long[]{0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0},
+                    new long[]{0, 0, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1},
+            }, GF3), GF3.getClass(),
+            toFieldMatrix(new long[][]{
+                    new long[]{1, 3, 9, 5, 15, 23, 13, 17, 20, 4, 12, 14},
+                    new long[]{1, 9, 15, 13, 20, 12, 11, 6, 7, 16, 22, 8},
+                    new long[]{1, 5, 13, 4, 11, 18, 16, 10, 25, 3, 15, 17},
+                    new long[]{1, 15, 20, 11, 7, 22, 25, 9, 13, 12, 6, 16},
+            }, GF_3_3), GF_3_3.getClass());
 }
