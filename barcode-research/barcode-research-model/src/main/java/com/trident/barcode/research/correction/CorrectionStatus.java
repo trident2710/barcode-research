69a1f9ab979d6954f2fb7a83832b67d8c6f92480
andriidy <andriidy@wix.com>
2023-02-05 23:06:04 +0200

    Barcode. BCH correction strategy

--- barcode-research/barcode-research-model/src/main/java/com/trident/barcode/research/correction/CorrectionStatus.java
+++ barcode-research/barcode-research-model/src/main/java/com/trident/barcode/research/correction/CorrectionStatus.java
@@ -3,5 +3,7 @@ package com.trident.barcode.research.correction;
 public enum CorrectionStatus {
     NO_ERROR,
     ERROR_CORRECTED,
-    ERROR_DETECTED
+    ERROR_DETECTED,
+
+    FAILURE,
 }
---
+++ barcode-research/barcode-research-model/src/main/java/com/trident/barcode/research/correction/CorrectionStrategies.java
@@ -0,0 +1,11 @@
+package com.trident.barcode.research.correction;
+
+import com.trident.math.bch.BCHCodes;
+
+public final class CorrectionStrategies {
+
+    public static final CorrectionStrategy BCH_9_3 = new BCHCorrectionStrategy<>(BCHCodes.BCH_GF4_9_3);
+
+    private CorrectionStrategies() {
+    }
+}
