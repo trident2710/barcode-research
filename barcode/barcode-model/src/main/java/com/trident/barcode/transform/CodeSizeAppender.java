84b47fff20bcb668056fa46f43b6d765ca37ecbc
andriidy <andriidy@wix.com>
2023-05-20 22:21:07 +0300

    Code size appender. Size digits auto determination

--- barcode/barcode-model/src/main/java/com/trident/barcode/transform/CodeSizeAppender.java
+++ barcode/barcode-model/src/main/java/com/trident/barcode/transform/CodeSizeAppender.java
@@ -1,13 +1,15 @@
 package com.trident.barcode.transform;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.trident.barcode.model.Barcode;
 import com.trident.barcode.model.BarcodeDictionary;
 import com.trident.barcode.model.BarcodeSign;
 import com.trident.barcode.model.ImmutableBarcode;
-import com.trident.barcode.transform.BarcodeTransformer;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,9 +18,9 @@ public class CodeSizeAppender implements BarcodeTransformer {
     private final BarcodeDictionary barcodeDictionary;
     private final int codeSizeSignCount;
 
-    public CodeSizeAppender(BarcodeDictionary barcodeDictionary, int codeSizeSignCount) {
+    public CodeSizeAppender(BarcodeDictionary barcodeDictionary) {
         this.barcodeDictionary = barcodeDictionary;
-        this.codeSizeSignCount = codeSizeSignCount;
+        this.codeSizeSignCount = calculateCodeSizeSignCount(barcodeDictionary);
     }
 
     @Override
@@ -30,6 +32,12 @@ public class CodeSizeAppender implements BarcodeTransformer {
         signsWithCodeSizeFirst.addAll(barcode.signs());
         return ImmutableBarcode.copyOf(barcode)
                 .withSigns(signsWithCodeSizeFirst);
+    }
+
+    @VisibleForTesting
+    static int calculateCodeSizeSignCount(BarcodeDictionary barcodeDictionary) {
+        int elementsCount = barcodeDictionary.charset(barcodeDictionary.defaultCharset()).signs().size() - 1;
+        return BigDecimal.valueOf(Math.log10(elementsCount)).setScale(0, RoundingMode.UP).intValue();
     }
 
     private List<BarcodeSign> encodeCodeSize(int codeSize) {
--- barcode/barcode-model/src/test/java/com/trident/barcode/transform/CodeSizeAppenderTest.java
+++ barcode/barcode-model/src/test/java/com/trident/barcode/transform/CodeSizeAppenderTest.java
@@ -5,9 +5,13 @@ import com.trident.barcode.model.ImmutableBarcode;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CodeSizeAppenderTest {
+
+    @Test
+    void testCodeSizeDigitsCount() {
+        assertEquals(2, CodeSizeAppender.calculateCodeSizeSignCount(BarcodeDictionaries.BASE_41));
+    }
 
     @Test
     void test() {
@@ -18,49 +22,15 @@ class CodeSizeAppenderTest {
                 .addSigns(BarcodeDictionaries.BASE_41.findSign("C").orElseThrow())
                 .build();
 
-        var appender = new CodeSizeAppender(BarcodeDictionaries.BASE_41, 1);
-
-        var expected = ImmutableBarcode.builder()
-                .dictionary(BarcodeDictionaries.BASE_41)
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("3").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("A").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("B").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("C").orElseThrow())
-                .build();
-
-        assertEquals(expected, appender.transform(barcode));
-    }
-
-    @Test
-    void testSize2DigitsLength1Digit() {
-        var barcode = ImmutableBarcode.builder()
-                .dictionary(BarcodeDictionaries.BASE_41)
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("1").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("2").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("3").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("4").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("5").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("6").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("7").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("8").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("9").orElseThrow())
-                .build();
-
-        var appender = new CodeSizeAppender(BarcodeDictionaries.BASE_41, 2);
+        var appender = new CodeSizeAppender(BarcodeDictionaries.BASE_41);
 
         var expected = ImmutableBarcode.builder()
                 .dictionary(BarcodeDictionaries.BASE_41)
                 .addSigns(BarcodeDictionaries.BASE_41.findSign("0").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("9").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("1").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("2").orElseThrow())
                 .addSigns(BarcodeDictionaries.BASE_41.findSign("3").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("4").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("5").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("6").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("7").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("8").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("9").orElseThrow())
+                .addSigns(BarcodeDictionaries.BASE_41.findSign("A").orElseThrow())
+                .addSigns(BarcodeDictionaries.BASE_41.findSign("B").orElseThrow())
+                .addSigns(BarcodeDictionaries.BASE_41.findSign("C").orElseThrow())
                 .build();
 
         assertEquals(expected, appender.transform(barcode));
@@ -82,7 +52,7 @@ class CodeSizeAppenderTest {
                 .addSigns(BarcodeDictionaries.BASE_41.findSign("0").orElseThrow())
                 .build();
 
-        var appender = new CodeSizeAppender(BarcodeDictionaries.BASE_41, 2);
+        var appender = new CodeSizeAppender(BarcodeDictionaries.BASE_41);
 
         var expected = ImmutableBarcode.builder()
                 .dictionary(BarcodeDictionaries.BASE_41)
@@ -102,26 +72,4 @@ class CodeSizeAppenderTest {
 
         assertEquals(expected, appender.transform(barcode));
     }
-
-    @Test
-    void testSize1DigitLength2Digits() {
-        var barcode = ImmutableBarcode.builder()
-                .dictionary(BarcodeDictionaries.BASE_41)
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("1").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("2").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("3").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("4").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("5").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("6").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("7").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("8").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("9").orElseThrow())
-                .addSigns(BarcodeDictionaries.BASE_41.findSign("0").orElseThrow())
-                .build();
-
-        var appender = new CodeSizeAppender(BarcodeDictionaries.BASE_41, 1);
-
-        assertThrows(IllegalArgumentException.class, () -> appender.transform(barcode));
-    }
-
 }
