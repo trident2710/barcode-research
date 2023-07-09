cb34bd6a96dddb389666346231f1e8045a0cb684
andriidy <andriidy@wix.com>
2023-07-09 20:22:44 +0300

    Add percentiles to http metrics. Updated load scripts

--- barcode-generation-service/scripts/loadx1000.sh
+++ barcode-generation-service/scripts/loadx1000.sh
@@ -2,13 +2,16 @@
 
 for (( i = 0; i < 10000; i++ )); do
 
+    msg_length=100
+
+    msg=$(cat /dev/urandom | env LC_ALL=C tr -dc 'A-Z0-9' | fold -w $msg_length | head -n 1)
+
     random_number=$(( RANDOM % 10 + 1 ))
 
-    # Sleep for the random number of milliseconds
     sleep_time=$(echo "scale=3; $random_number / 1000" | bc)
 
-    echo "Slept for $sleep_time seconds"
+    echo "Generating barcode for: $msg"
     sleep "$sleep_time"
     curl --request GET -sL \
-         --url 'http://localhost:8080/barcode/generate?message=ABCDFGNSDGSNFGSEIOGJNPRIGVJVGSDPFNEDPIFNEPFIENWFPIWENFPWEJFWEPFJWEPFJWEPFNWEFPIWEHFPIWEJFWPEIFNWEPFWEPIFJWEPFHJWEP' &
+         --url "http://localhost:8080/barcode/generate?message=$msg" &
 done
