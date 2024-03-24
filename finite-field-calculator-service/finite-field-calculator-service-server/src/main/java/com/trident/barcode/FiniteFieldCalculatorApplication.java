55e42d7659b81aead5494aa5cf97da2d27ab2fce
andriidy <andriidy@wix.com>
2024-03-24 17:07:10 +0200

    Split finite-field-calculator and barcode-generator service. Add icon. Change deployment scripts

---
+++ finite-field-calculator-service/finite-field-calculator-service-server/src/main/java/com/trident/barcode/FiniteFieldCalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.trident.barcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiniteFieldCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiniteFieldCalculatorApplication.class, args);
+    }
+}
