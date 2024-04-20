0c4f75605fa92d522a9cc0b32ccb0569654bf78b
andriidy <andriidy@wix.com>
2024-04-20 21:20:38 +0300

    Add reed-solomon operations components

--- finite-field-calculator-service/finite-field-calculator-service-ui/src/app/app.routes.ts
+++ finite-field-calculator-service/finite-field-calculator-service-ui/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import {Routes} from '@angular/router';
 import {PolynomOperationsComponent} from "./components/polynom-operations/polynom-operations.component";
 import {OtherOperationsComponent} from "./components/other-operations/other-operations.component";
 import {GfpOperationsComponent} from "./components/gfp-operations/gfp-operations.component";
+import {ReedSolomonOperationsComponent} from "./components/reed-solomon-operations/reed-solomon-operations.component";
 
 export const routes: Routes = [
   {path: "", redirectTo: "gfOperations", pathMatch: "full"},
   {path: "gfOperations", component: GfpOperationsComponent},
   {path: "polynomOperations", component: PolynomOperationsComponent},
+  {path: "reedSolomonOperations", component: ReedSolomonOperationsComponent},
   {path: "otherOperations", component: OtherOperationsComponent}
 ];
---
+++ finite-field-calculator-service/finite-field-calculator-service-ui/src/app/components/reed-solomon-operations/reed-solomon-operations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'reed-solomon-operations',
+  standalone: true,
+  imports: [],
+  templateUrl: './reed-solomon-operations.component.html',
+  styleUrl: './reed-solomon-operations.component.css'
+})
+export class ReedSolomonOperationsComponent {
+
+}
